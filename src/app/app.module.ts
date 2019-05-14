f2349722fb884a1704a2c01fc19b05c95e008603
Kwame Baah-Apraku <Kwame.Baah-Apraku@aarcorp.com>
2019-05-14 03:27:48 -0500

    Added Services File

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +15,23 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ApolloModule,
+    HttpLinkModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(
+    apollo: Apollo,
+    httpLink: HttpLink) 
+    { 
+      apollo.create({
+        link: httpLink.create({ uri: environment.graphqlEndpoint }),
+        cache: new InMemoryCache()
+      });
+    }
+ }
