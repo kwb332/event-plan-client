28fad4282d177e907dd0ed1adc036b926fb8ae79
Kwame Baah-Apraku <Kwame.Baah-Apraku@aarcorp.com>
2019-05-15 09:53:45 -0500

    Added BootStrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import { AlertModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,6 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    NgbModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    AlertModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
