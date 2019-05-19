e62814c21ec4296c153280196ebbfc4e35a346a0
Kwame Baah-Apraku <Kwame.Baah-Apraku@aarcorp.com>
2019-05-18 21:55:43 -0500

    Fixed App Name

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/raggioeventplanclient'));
+app.use(express.static(__dirname + '/dist/"event-plan-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/raggioeventplanclient/index.html'));
+res.sendFile(path.join(__dirname+'/dist/"event-plan-app/index.html'));
 });
 
-//const path = require('path');
-//app.get('/*', function(req,res) {
-//res.sendFile(path.join(__dirname+'/dist/index.html'));
-//});
+const path = require('path');
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
