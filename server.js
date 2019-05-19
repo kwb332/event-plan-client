64208d0902530456175dd8b8c1f8f7d4ca148e69
Kwame Baah-Apraku <Kwame.Baah-Apraku@aarcorp.com>
2019-05-18 22:00:57 -0500

    New path

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/"event-plan-app/index.html'));
 });
 
-const path = require('path');
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
+//const path = require('path');
+//app.get('/*', function(req,res) {
+//res.sendFile(path.join(__dirname+'/dist/index.html'));
+//});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
