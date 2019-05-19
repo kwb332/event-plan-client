236a51d55878eb4175bdb4866849d79ed52e981e
Kwame Baah-Apraku <Kwame.Baah-Apraku@aarcorp.com>
2019-05-18 22:14:58 -0500

    Fixed Paths again and again

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/"event-plan-app'));
-
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/event-plan-app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
-
-//const path = require('path');
 //app.get('/*', function(req,res) {
-//res.sendFile(path.join(__dirname+'/dist/index.html'));
+    
+//res.sendFile(path.join(__dirname+'/dist/event-plan-app/index.html'));
 //});
+
+
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
