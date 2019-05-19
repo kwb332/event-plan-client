1fc4898e0abf16186ef072baeeb0bc41b89c3246
Kwame Baah-Apraku <Kwame.Baah-Apraku@aarcorp.com>
2019-05-18 22:27:40 -0500

    gdfgdfgdf

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/"event-plan-app'));
 
-//app.get('/*', function(req,res) {
+app.get('/*', function(req,res) {
     
-//res.sendFile(path.join(__dirname+'/dist/event-plan-app/index.html'));
-//});
+res.sendFile(path.join(__dirname+'/dist/event-plan-app/index.html'));
+});
 
 //const path = require('path');
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
+//app.get('/*', function(req,res) {
+//res.sendFile(path.join(__dirname+'/dist/index.html'));
+//});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
